708d5725d132dede70d6911e60a39c287348b83c
AlexYangLong <37995431+AlexYangLong@users.noreply.github.com>
2019-01-15 16:54:56 +0800

    Create status_code.py

---
+++ status_code.py
@@ -0,0 +1,17 @@
+# 公共
+SUCCESS_CODE = 601
+SUCCESS_MSG_CN = "操作成功"
+SUCCESS_MSG_EN = "Operate successful."
+
+FAIL_CODE = 602
+FAIL_MSG_CN = "操作失败,请重试"
+FAIL_MSG_EN = "Operate failed."
+
+REQUIRED_PARAM_CODE = 603
+REQUIRED_PARAM_MSG_CN = "请求参数{}不能为空"
+REQUIRED_PARAM_MSG_EN = "Request parameter {} can not be empty."
+
+# 用户相关
+JSON_PARAMS_ERROR = {"code": 1001, "msg_cn": "JSON请求参数错误", "msg_en": "JSON request parameter error."}
+USER_ERROR = {"code": 1002, "msg_cn": "帐号或密码错误", "msg_en": "Account or password error."}
+
