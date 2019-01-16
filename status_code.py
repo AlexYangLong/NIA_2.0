77ea33975eaf3b8454f34f4f2219194fa30f4e9c
AlexYangLong <37995431+AlexYangLong@users.noreply.github.com>
2019-01-16 17:00:42 +0800

    Update status_code.py

--- status_code.py
+++ status_code.py
@@ -1,17 +1,28 @@
 # 公共
+SUCCESS = {"code": 601, "msg_cn": "操作成功", "msg_en": "Operate successful."}
 SUCCESS_CODE = 601
-SUCCESS_MSG_CN = "操作成功"
-SUCCESS_MSG_EN = "Operate successful."
 
+FAIL = {"code": 602, "msg_cn": "操作失败,请重试", "msg_en": "Operate failed."}
 FAIL_CODE = 602
-FAIL_MSG_CN = "操作失败,请重试"
-FAIL_MSG_EN = "Operate failed."
 
 REQUIRED_PARAM_CODE = 603
 REQUIRED_PARAM_MSG_CN = "请求参数{}不能为空"
 REQUIRED_PARAM_MSG_EN = "Request parameter {} can not be empty."
 
+URL_ERROR = {"code": 604, "msg_cn": "请求路径或方式错误", "msg_en": "Request path or method error."}
+
+JSON_PARAMS_ERROR = {"code": 605, "msg_cn": "JSON请求参数错误", "msg_en": "JSON request parameter error."}
+ARGS_PARAMS_ERROR = {"code": 606, "msg_cn": "ARGS请求参数错误", "msg_en": "ARGS request parameter error."}
+
+NO_LOGIN = {"code": 607, "msg_cn": "用户未登录", "msg_en": "Users are not logged in."}
+
 # 用户相关
-JSON_PARAMS_ERROR = {"code": 1001, "msg_cn": "JSON请求参数错误", "msg_en": "JSON request parameter error."}
-USER_ERROR = {"code": 1002, "msg_cn": "帐号或密码错误", "msg_en": "Account or password error."}
+USER_ERROR = {"code": 1001, "msg_cn": "帐号或密码错误", "msg_en": "Account or password error."}
+USER_NOT_EXIST = {"code": 1002, "msg_cn": "用户不存在", "msg_en": "User is not existed."}
+USER_ACCOUNT_EXIST = {"code": 1003, "msg_cn": "帐户名已存在", "msg_en": "User account is already existed."}
+PASSWORD_ERROR = {"code": 1004, "msg_cn": "密码错误", "msg_en": "Password error."}
+USER_IS_NOT_ACTIVE = {"code": 1005, "msg_cn": "用户未激活", "msg_en": "User is not active."}
+
+# 随笔相关
+ESSAY_NOT_EXIST = {"code": 1101, "msg_cn": "随笔不存在", "msg_en": "Essay is not existed."}
 
