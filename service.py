9d7c97aaabd05cc14957b5c1f5f02d3636a78966
AlexYangLong <37995431+AlexYangLong@users.noreply.github.com>
2019-01-16 16:59:40 +0800

    Update service.py

--- service.py
+++ service.py
@@ -3,6 +3,7 @@ from contextlib import contextmanager
 from sqlalchemy.orm import sessionmaker
 
 from Src.common import model
+from Src.common.model import LogInfo
 
 Session = sessionmaker(bind=model.engine)
 
@@ -22,6 +23,22 @@ def session_scope():
         raise Exception(ex)
     finally:
         session.close()
+
+
+class LogService(object):
+    @staticmethod
+    def write_log(client_ip, action_cn, action_en, result_cn, result_en, reason=None, user_id=None):
+        log = LogInfo({
+            "client_ip": client_ip,
+            "action_cn": action_cn,
+            "action_en": action_en,
+            "result_cn": result_cn,
+            "result_en": result_en,
+            "reason": reason,
+            "user_id": user_id
+        })
+        with session_scope() as session:
+            session.add(log)
 
 
 # class Message(object):
