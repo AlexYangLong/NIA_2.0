819fc16684fe03b8fcd0928e5559e3f22934af7d
AlexYangLong <37995431+AlexYangLong@users.noreply.github.com>
2019-01-18 11:22:56 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,6 +1,25 @@
+from flask import render_template, jsonify
+
+from Src.utils import log as logger
+from Src.utils.decorator import write_operate_log
 from Src.utils.init_app import create_app
 
 app = create_app()
 
+logger.init("log")
+
+
+@app.route(r"/v1/bms/index.html")
+def index():
+    return render_template("nia_web/index.html")
+
+
+@app.route(r"/test/")
+@write_operate_log
+def test():
+    return {"code": 200, "msg": "aaa"}
+
+
 if __name__ == '__main__':
+    logger.info("启动并初始化成功")
     app.run(port=5001)
