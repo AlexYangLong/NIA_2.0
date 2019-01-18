e7a322457ecb44c9efca151b043da0d179122401
AlexYangLong <37995431+AlexYangLong@users.noreply.github.com>
2019-01-18 11:19:26 +0800

    Update user_controller.py

--- user_controller.py
+++ user_controller.py
@@ -3,8 +3,10 @@ from flask_restful import Resource
 
 from Src.common import status_code
 from Src.common.constant import USER_REQUIRED_FIELD
+from Src.common.service import get_reset_password
 from Src.services.user_service import UserService
 from Src.utils import check_utils
+from Src.utils.decorator import login_required, user_is_active
 
 
 class RegisterController(Resource):
@@ -74,17 +76,13 @@ class LoginController(Resource):
 
 class UserInfoController(Resource):
     @staticmethod
+    @login_required
+    @user_is_active
     def get(uid=None):
         if not check_utils.check_param_format(request.path, [r"^/v1/bms/user/([1-9][0-9]*)/$", r"^/v1/bms/user/$"]):
             return jsonify(status_code.URL_ERROR)
-        user_id = session.get("user_id")
-        if not user_id:
-            return jsonify(status_code.NO_LOGIN)
         try:
             user_service = UserService()
-            current_user = user_service.get_user_by_id(user_id=user_id)
-            if not current_user.get("is_active"):
-                return jsonify(status_code.USER_IS_NOT_ACTIVE)
             if uid:
                 user = user_service.get_user_by_id(user_id=uid)
                 if not user:
@@ -102,16 +100,11 @@ class UserInfoController(Resource):
             return jsonify(status_code.FAIL)
 
     @staticmethod
+    @login_required
+    @user_is_active
     def post(uid=None):
         if not check_utils.check_param_format(request.path, [r"^/v1/bms/user/$"]):
             return jsonify(status_code.URL_ERROR)
-        user_id = session.get("user_id")
-        if not user_id:
-            return jsonify(status_code.NO_LOGIN)
-        user_service = UserService()
-        current_user = user_service.get_user_by_id(user_id=user_id)
-        if not current_user.get("is_active"):
-            return jsonify(status_code.USER_IS_NOT_ACTIVE)
         data = request.json
         if not (data and isinstance(data, dict)):
             return jsonify(status_code.JSON_PARAMS_ERROR)
@@ -123,6 +116,7 @@ class UserInfoController(Resource):
         # 正则验证
 
         try:
+            user_service = UserService()
             user = user_service.get_user_by_account(account=data.get("account"))
             if user:
                 return jsonify(status_code.USER_ACCOUNT_EXIST)
@@ -136,16 +130,11 @@ class UserInfoController(Resource):
             return jsonify(status_code.FAIL)
 
     @staticmethod
+    @login_required
+    @user_is_active
     def put(uid=None):
         if not check_utils.check_param_format(request.path, [r"^/v1/bms/user/([1-9][0-9]*)/$"]):
             return jsonify(status_code.URL_ERROR)
-        user_id = session.get("user_id")
-        if not user_id:
-            return jsonify(status_code.NO_LOGIN)
-        user_service = UserService()
-        current_user = user_service.get_user_by_id(user_id=user_id)
-        if not current_user.get("is_active"):
-            return jsonify(status_code.USER_IS_NOT_ACTIVE)
         data = request.json
         if not (data and isinstance(data, dict)):
             return jsonify(status_code.JSON_PARAMS_ERROR)
@@ -154,6 +143,7 @@ class UserInfoController(Resource):
                             "msg_cn": status_code.REQUIRED_PARAM_MSG_CN.format("电话号码"),
                             "msg_en": status_code.REQUIRED_PARAM_MSG_EN.format("phone")})
         try:
+            user_service = UserService()
             user = user_service.get_user_by_id(user_id=uid)
             if not user:
                 return jsonify(status_code.USER_NOT_EXIST)
@@ -166,16 +156,11 @@ class UserInfoController(Resource):
             return jsonify(status_code.FAIL)
 
     @staticmethod
+    @login_required
+    @user_is_active
     def patch(uid=None):
         if not check_utils.check_param_format(request.path, [r"^/v1/bms/user/$"]):
             return jsonify(status_code.URL_ERROR)
-        user_id = session.get("user_id")
-        if not user_id:
-            return jsonify(status_code.NO_LOGIN)
-        user_service = UserService()
-        current_user = user_service.get_user_by_id(user_id=user_id)
-        if not current_user.get("is_active"):
-            return jsonify(status_code.USER_IS_NOT_ACTIVE)
         data = request.json
         if not (data and isinstance(data, dict)) or not isinstance(data.get("ids"), list):
             return jsonify(status_code.JSON_PARAMS_ERROR)
@@ -192,6 +177,7 @@ class UserInfoController(Resource):
                             "msg_cn": "激活状态参数不是一个布尔值",
                             "msg_en": "The activation state parameter is not a Boolean value."})
         try:
+            user_service = UserService()
             user_service.change_user_status(is_active=data.get("is_active"), user_id_list=data.get("ids"))
             return jsonify(status_code.SUCCESS)
         except Exception as ex:
@@ -199,16 +185,11 @@ class UserInfoController(Resource):
             return jsonify(status_code.FAIL)
 
     @staticmethod
+    @login_required
+    @user_is_active
     def delete(uid=None):
         if not check_utils.check_param_format(request.path, [r"^/v1/bms/user/$"]):
             return jsonify(status_code.URL_ERROR)
-        user_id = session.get("user_id")
-        if not user_id:
-            return jsonify(status_code.NO_LOGIN)
-        user_service = UserService()
-        current_user = user_service.get_user_by_id(user_id=user_id)
-        if not current_user.get("is_active"):
-            return jsonify(status_code.USER_IS_NOT_ACTIVE)
         data = request.json
         if not (data and isinstance(data, dict)) or not isinstance(data.get("ids"), list):
             return jsonify(status_code.JSON_PARAMS_ERROR)
@@ -217,6 +198,7 @@ class UserInfoController(Resource):
                             "msg_cn": status_code.REQUIRED_PARAM_MSG_CN.format("用户id"),
                             "msg_en": status_code.REQUIRED_PARAM_MSG_EN.format("user ids")})
         try:
+            user_service = UserService()
             user_service.delete_user(user_id_list=data.get("ids"))
             return jsonify(status_code.SUCCESS)
         except Exception as ex:
@@ -226,16 +208,12 @@ class UserInfoController(Resource):
 
 class UserPwdController(Resource):
     @staticmethod
-    def patch(uid=None):
-        if not check_utils.check_param_format(request.path, [r"^/v1/bms/user/([1-9][0-9]*)/password/$"]):
+    @login_required
+    @user_is_active
+    def patch():
+        if not check_utils.check_param_format(request.path, [r"^/v1/bms/user/password/$"]):
             return jsonify(status_code.URL_ERROR)
         user_id = session.get("user_id")
-        if not user_id:
-            return jsonify(status_code.NO_LOGIN)
-        user_service = UserService()
-        current_user = user_service.get_user_by_id(user_id=user_id)
-        if not current_user.get("is_active"):
-            return jsonify(status_code.USER_IS_NOT_ACTIVE)
         data = request.json
         if not (data and isinstance(data, dict)):
             return jsonify(status_code.JSON_PARAMS_ERROR)
@@ -248,13 +226,33 @@ class UserPwdController(Resource):
                             "msg_cn": status_code.REQUIRED_PARAM_MSG_CN.format("新密码"),
                             "msg_en": status_code.REQUIRED_PARAM_MSG_EN.format("new password")})
         try:
-            user = user_service.get_user_by_id(user_id=uid)
+            user_service = UserService()
+            user = user_service.get_user_by_id(user_id=user_id)
             if not user:
                 return jsonify(status_code.USER_NOT_EXIST)
-            if not user_service.check_user_password(user_id=uid, password=data.get("old_password")):
+            if not user_service.check_user_password(user_id=user_id, password=data.get("old_password")):
                 return jsonify(status_code.PASSWORD_ERROR)
-            user_service.update_user_password(user_id=uid, new_password=data.get("new_password"))
+            user_service.update_user_password(user_id=user_id, new_password=data.get("new_password"))
             return jsonify(status_code.SUCCESS)
+        except Exception as ex:
+            print(ex)
+            return jsonify(status_code.FAIL)
+
+
+class ResetPwdController(Resource):
+    @staticmethod
+    @login_required
+    @user_is_active
+    def patch(uid=None):
+        if not check_utils.check_param_format(request.path, [r"^/v1/bms/user/([1-9][0-9]*)/password/$"]):
+            return jsonify(status_code.URL_ERROR)
+        try:
+            user_service = UserService()
+            new_password = get_reset_password()
+            user_service.update_user_password(user_id=uid, new_password=new_password)
+            result = status_code.SUCCESS
+            result["new_password"] = new_password
+            return jsonify(result)
         except Exception as ex:
             print(ex)
             return jsonify(status_code.FAIL)
