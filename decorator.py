1fa1eff1e14ba223b6cef62a0e6bc6a1bec8ad4b
AlexYangLong <37995431+AlexYangLong@users.noreply.github.com>
2019-01-21 11:19:04 +0800

    Update decorator.py

--- decorator.py
+++ decorator.py
@@ -1,21 +1,33 @@
 from functools import wraps
 
-from flask import jsonify, session
+from flask import jsonify, session, request
 
 from Src.common import status_code
 from Src.common.service import LogService
 from Src.services.user_service import UserService
-from Src.utils import log as logger
 
 
 def login_required(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        if not session.get("user_id"):
-            return jsonify(status_code.NO_LOGIN)
+        if not (session and session.get("user_id") and session.get("token")):
+            return status_code.USER_NO_LOGIN
+        try:
+            user_service = UserService()
+            user = user_service.get_user_by_id(session.get("user_id"))
+            if not user:
+                return status_code.USER_NOT_EXIST
+            if not user.get("is_active"):
+                return status_code.USER_IS_NOT_ACTIVE
+            if user.get("token") != session.get("token"):
+                return status_code.PLEASE_LOGIN
+        except Exception as ex:
+            print(ex)
+            return status_code.FAIL
 
         return func(*args, **kwargs)
     return wrapper
+
 
 def user_is_active(func):
     @wraps(func)
@@ -32,16 +44,30 @@ def user_is_active(func):
     return wrapper
 
 
-def write_operate_log(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        result = func(*args, **kwargs)
-        # 在这里写入日志文件
-        if result.get("code") == 200:
-            # LogService().write_log()
-            print("aaa")
-        else:
-            print("bbb")
-        return jsonify(result)
+def write_operate_log(action_cn="", action_en=""):
+    def wrapper(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            # print(action_cn, action_en)
+            ip = request.remote_addr
+            result = func(*args, **kwargs)
 
+            if session and session.get("user_id"):
+                user_id = session.get("user_id")
+            else:
+                user_id = None
+            # 在这里操作日志
+            if result.get("code") == status_code.SUCCESS_CODE:
+                result_cn = "成功"
+                result_en = "SUCCESS"
+            else:
+                result_cn = "失败"
+                result_en = "FAIL"
+            LogService().write_log(client_ip=ip, action_cn=action_cn, action_en=action_en,
+                                   result_cn=result_cn, result_en=result_en,
+                                   reason_cn=result.get("msg_cn"), reason_en=result.get("msg_en"),
+                                   user_id=user_id)
+            return jsonify(result)
+
+        return inner
     return wrapper
