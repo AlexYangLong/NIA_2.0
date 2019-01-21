15befda2e9a715e31a411a4fd4bf024dd5592514
AlexYangLong <37995431+AlexYangLong@users.noreply.github.com>
2019-01-21 11:21:00 +0800

    Update status_code.py

--- status_code.py
+++ status_code.py
@@ -14,7 +14,8 @@ URL_ERROR = {"code": 604, "msg_cn": "请求路径或方式错误", "msg_en": "Re
 JSON_PARAMS_ERROR = {"code": 605, "msg_cn": "JSON请求参数错误", "msg_en": "JSON request parameter error."}
 ARGS_PARAMS_ERROR = {"code": 606, "msg_cn": "ARGS请求参数错误", "msg_en": "ARGS request parameter error."}
 
-NO_LOGIN = {"code": 607, "msg_cn": "用户未登录", "msg_en": "Users are not logged in."}
+USER_NO_LOGIN = {"code": 607, "msg_cn": "用户未登录", "msg_en": "Users are not logged in."}
+PLEASE_LOGIN = {"code": 608, "msg_cn": "请先登录", "msg_en": "Please login first."}
 
 # 用户相关
 USER_ERROR = {"code": 1001, "msg_cn": "帐号或密码错误", "msg_en": "Account or password error."}
@@ -22,7 +23,10 @@ USER_NOT_EXIST = {"code": 1002, "msg_cn": "用户不存在", "msg_en": "User is 
 USER_ACCOUNT_EXIST = {"code": 1003, "msg_cn": "帐户名已存在", "msg_en": "User account is already existed."}
 PASSWORD_ERROR = {"code": 1004, "msg_cn": "密码错误", "msg_en": "Password error."}
 USER_IS_NOT_ACTIVE = {"code": 1005, "msg_cn": "用户未激活,请联系管理员", "msg_en": "User is not active."}
+ACTIVE_NOT_BOOL = {"code": 1006, "msg_cn": "激活状态参数不是一个布尔值", "msg_en": "The activation state parameter is not a Boolean value."}
 
 # 随笔相关
 ESSAY_NOT_EXIST = {"code": 1101, "msg_cn": "随笔不存在", "msg_en": "Essay is not existed."}
+ESSAY_STATUS_NOT_INT = {"code": 1102, "msg_cn": "随笔状态值不是整数", "msg_en": "Essay status is not integer."}
+ESSAY_STATUS_OUT_RANGE = {"code": 1103, "msg_cn": "随笔状态值超出范围", "msg_en": "Essay status is out of range."}
 
