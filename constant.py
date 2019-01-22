630a671be19021aa561c2070c118187eab6e5647
AlexYangLong <37995431+AlexYangLong@users.noreply.github.com>
2019-01-22 11:00:36 +0800

    Update constant.py

--- constant.py
+++ constant.py
@@ -2,3 +2,10 @@ RANDOM_CHAR = "qwertyuiopasdfghjklzxcvbnm123456789QWERTYUIOPASDFGHJKLZXCVBNM@#$*
 
 USER_REQUIRED_FIELD = {"account": "用户帐号", "password": "用户密码", "phone": "用户电话号码"}
 ESSAY_REQUIRED_FIELD = {"title": "随笔标题", "content": "随笔内容"}
+COMMENT_REQUIRED_FIELD = {"article_id": "评论文章", "article_type": "评论种类", "comment_con": "评论内容"}
+
+ESSAY_STATUS = {
+    "PUBLIC": 1,
+    "ONLY_FRIEND": 2,
+    "PRIVATE": 3
+}
